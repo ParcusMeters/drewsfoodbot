dda2a94fd87e4c4d17a39df9fbb56cb0679a9d54
marcus <mpet0303@uni.sydney.edu.au>
2022-09-25 20:46:11 +1000

    added greeting

--- services/receive.js
+++ services/receive.js
@@ -222,9 +222,9 @@ module.exports = class Receive {
     }else if (payload === "CAN I SEE THE MENU?"){
       response = Response.genMenuMessage(this.user);
     }else if (payload === "TODAY’S MENU"){
-      response = Response.genMenuMessage(this.user);
+      response = Order.handlePayload("TODAYS_MENU");
     }else if (payload === "TOMORROW’S MENU"){
-      response = Response.genMenuMessage(this.user);
+      response = Order.handlePayload("TOMORROWS_MENU");
     }
     else {
       response = {
